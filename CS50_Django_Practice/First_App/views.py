c26b4ce26e3d6d36dd5d7c2c9ce802f0b5f89bcd
Rezwan Ahmed <maxtalon94@gmail.com>
2022-07-03 18:46:15 +0800

    day 2 of learning django

--- CS50_Django_Practice/First_App/views.py
+++ CS50_Django_Practice/First_App/views.py
@@ -1,3 +1,9 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return HttpResponse("This is my first web application!")
+
+def greet(request, name):
+    return HttpResponse(f"Hello {name}! Welcome to my first web application!")
