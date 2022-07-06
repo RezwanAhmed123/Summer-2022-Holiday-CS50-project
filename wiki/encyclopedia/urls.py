a86e48e3648130edc92ab960b9f734c2f5f663c1
Rezwan Ahmed <maxtalon94@gmail.com>
2022-07-07 01:52:13 +0800

    finally managed to implement search successfully, order of the urls matter

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -6,6 +6,6 @@ app_name = "encyclopedia"
 urlpatterns = [
     path("", views.index, name="index"),
     path("newpage",views.newpage, name="newpage"),
-    path("<str:entry>", views.entry_page, name="entry_page"),
-    path("errorpage",views.errorpage, name="error_page")
+    path("search", views.search, name="search"),
+    path("<str:entry>", views.entry_page, name="entry_page")
 ]
--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -10,9 +10,13 @@ class NewPageForm(forms.Form):
     title = forms.CharField(label="", widget=forms.TextInput(attrs={"placeholder": "Name of new page"}))
     content = forms.CharField(label="", widget=forms.Textarea(attrs={"placeholder": "Please input the content of the webpage using MARKDOWN only!"}))
 
+class SearchExistingPage(forms.Form):
+    search_term = forms.CharField(label="", widget=forms.TextInput(attrs={"class":"search" , "placeholder": "Search Encyclopedia"}))
+
 def index(request):
     return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
+        "entries": util.list_entries(),
+        "search_term": SearchExistingPage()
     })
 
 def entry_page(request, entry):
@@ -20,13 +24,19 @@ def entry_page(request, entry):
     if entry_details != None:
         return render(request, "encyclopedia/entry_page.html",{
         "entry_name": entry,
-        "entry_details": entry_details
+        "entry_details": entry_details,
+        "search_term": SearchExistingPage()
         })
     else:
-        return render(request,"encyclopedia/errorpage.html")
-
-def errorpage(request):
-    return render(request, "encyclopedia/errorpage.html")
+        related_entries = set()
+        for filename in util.list_entries():
+            if entry in filename:
+                related_entries.add(filename)
+        return render(request,"encyclopedia/errorpage.html",{
+            "entry_name": entry,
+            "related_entries": related_entries,
+            "search_term": SearchExistingPage()
+        })
 
 def newpage(request):
     if request.method == "POST":
@@ -43,5 +53,17 @@ def newpage(request):
                 "form": form
                 })
     return render(request, "encyclopedia/newpage.html", {
-        "form": NewPageForm()
+        "form": NewPageForm(),
+        "search_term": SearchExistingPage()
     })
+
+def search(request):
+    if request.method == "POST":
+        search_term = SearchExistingPage(request.POST)
+        if search_term.is_valid():
+            entry_name = search_term.cleaned_data["search_term"]
+            return entry_page(request, entry_name)
+    else:
+        return render(request, "encyclopedia/index.html", {
+            "search_term": SearchExistingPage()
+        })
