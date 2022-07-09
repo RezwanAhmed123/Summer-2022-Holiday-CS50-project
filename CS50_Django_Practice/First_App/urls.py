2b29eadd8b356f6bea0831b52e5df4317283b179
Rezwan Ahmed <maxtalon94@gmail.com>
2022-07-09 21:36:07 +0800

    updated the names to make folders more organised

--- CS50_Django_Practice/First_App/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from . import views # The "." represents current directory
-
-urlpatterns = [
-    path("", views.index, name="index"),
-    path("<str:name>", views.greet, name="greetings")
-]
--- CS50_Django_Practice/New_Years/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class NewYearsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'New_Years'
